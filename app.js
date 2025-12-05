2396332bd2d18b2b848a5fc8830b4577ac6f6d05
jmdavis815-web <jmdavis815@gmail.com>
2025-12-05 14:04:47 -0600

    js and toasts for form submission feedback.

---
+++ app.js
@@ -0,0 +1,47 @@
+let url = "https://zmxykpzkidhegngzbcmr.supabase.co/rest/v1/leads";
+let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpteHlrcHpraWRoZWduZ3piY21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzkxOTksImV4cCI6MjA4MDQ1NTE5OX0.FRrzrLc0dqqDJR_e712OXYfTYNYW3v9WXH6T2FtGDn0";
+
+function showToastSuccess() {
+    const toastEl = document.getElementById("toastSuccess");
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
+
+function showToastError() {
+    const toastEl = document.getElementById("toastError");
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
+
+async function createLead(){
+
+    event.preventDefault();
+
+    let name = document.getElementById("contactName").value;
+    let email = document.getElementById("contactEmail").value;
+    let message = document.getElementById("contactMessage").value;
+
+    if(name === "" || email === "" || message === ""){ 
+        showToastError()
+        return;
+    }
+
+    let lead = {
+        "name" : name , 
+        "email" : email,
+        "message" : message
+    }
+
+    let response = await fetch(url , {
+        method: "POST",
+        headers: {
+            "apikey" : apikey,
+            "Content-Type" : "application/json"
+        }, 
+        body : JSON.stringify(lead)
+    })
+
+    console.log("Lead Created")
+    showToastSuccess()
+    
+}
